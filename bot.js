721b50ef0e9d7e4cf8a6c20629b1321678afadea
sh20raj <shaswatraj3@gmail.com>
2024-04-29 08:08:42 +0530

    ✅ Bot Working in HTML Mode

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const fetch = require('node-fetch');
+const axios = require('axios'); // Import Axios library
 require('dotenv').config(); // Load environment variables from .env file
 
 // Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token obtained from BotFather
@@ -12,26 +12,59 @@ const bot = new TelegramBot(token, { polling: true });
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text.toString().toLowerCase();
+  bot.sendMessage(chatId, messageText)
 
   // Check if the message contains a link
   if (messageText.includes('https://')) {
+
     const url = extractUrl(messageText);
 
     // Check if the URL is from teraboxapp.com
     if (url.includes('teraboxapp.com')) {
       try {
-        // Fetch data from terabox downloader API
-        const response = await fetch('https://ytshorts.savetube.me/api/v1/terabox-downloader', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ url }),
-        });
+        bot.sendMessage(chatId, "Wait........");
+        // Fetch data from terabox downloader API using Axios
+        const response = await fetch("https://ytshorts.savetube.me/api/v1/terabox-downloader", {
+            "headers": {
+              "accept": "application/json, text/plain, */*",
+              "accept-language": "en-US,en;q=0.9,hi;q=0.8",
+              "cache-control": "no-cache",
+              "content-type": "application/json",
+              "pragma": "no-cache",
+              "priority": "u=1, i",
+              "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+              "sec-ch-ua-mobile": "?0",
+              "sec-ch-ua-platform": "\"macOS\"",
+              "sec-fetch-dest": "empty",
+              "sec-fetch-mode": "cors",
+              "sec-fetch-site": "same-origin",
+              "cookie": "_ga=GA1.1.2110759286.1711701275; FCNEC=%5B%5B%22AKsRol9AEBYqsxFEeKI7ZfmIA6z6Kdr8jEVx3wLHKuXQjRTyy-TGf-Uq8vNhpew0dbd4KB04eejgrTAQEbdvA0CSC8nEvs-ZYssal2zC841o5adBmM9WkRQ2SVR0Tt2TqFw5gfxD3z0WN0eGYE4U-g62A_4yKalr3g%3D%3D%22%5D%5D; _ga_3Q4D9SLPKL=GS1.1.1714320325.5.1.1714320325.0.0.0; cf_clearance=Wm.0hFtXbDxA_owC3lvaD35Cp5RSvg_6kDaAotsJsag-1714320326-1.0.1.1-vOSYCYtc1sQb7f.CHmdymgp1L64WP4cVq0Ypmke.IA8MwmNAs0iKlrMbs7jxc2oXji94fIZKYTJZPThjo9llEA"
+            },
+            "referrerPolicy": "no-referrer",
+            "body": "{\"url\":\"https://teraboxapp.com/s/1g03a7bZ6wXMYHoEXQDd6sQ\"}",
+            "method": "POST"
+          });
+          let data = await response.json();
+          console.log(data);
+          // bot.sendMessage(chatId, data.response[0].resolutions);
+          // bot.sendMessage(chatId, data.response[0].thumbnail);
+          // bot.sendMessage(chatId, data.response[0].title);
 
+          let msgtemplate = `
+          <b>Title:</b> ${data.response[0].title}
+          <b>Thumbnail:</b> ${data.response[0].thumbnail}
+
+          <b>Download:</b> ${data.response[0].resolutions["Fast Download"]}
+          
+          `;
+          bot.sendMessage(chatId, msgtemplate, {
+            parse_mode: "HTML"
+          });
+
+        
         // Check if response is successful
-        if (response.ok) {
-          const data = await response.json();
+        if (response.status === 200) {
+          const data = response.data;
           // Send the returned data to the user
           bot.sendMessage(chatId, JSON.stringify(data));
         } else {
