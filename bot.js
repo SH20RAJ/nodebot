ca8a61fb71859003234d09eb7b73fe36a53ee90b
sh20raj <shaswatraj3@gmail.com>
2024-05-01 14:03:06 +0530

    imgjourney added

--- bot.js
+++ bot.js
@@ -89,6 +89,85 @@ app.post('/bot', (req, res) => {
     res.sendStatus(200);
 });
 
+
+app.post('/imgjourney', (req, res) => {
+    let manychat = fetch("https://wh.manychat.com/tgwh/tg0o83f4yg73hfgi73f2g89938g/6564625956/3cb9c43b300de42ccc337cc7d8b3e455ceef7d73",{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req.body)
+    })
+    console.log('Received message:', req.body.message);
+
+    // Initialize message variables
+    const message = req.body.message;
+    let textContent = message.text || (message.caption ? message.caption : '');
+
+    if (!textContent) {
+        console.log('No text or caption in the message');
+        return res.sendStatus(200);  // Acknowledge the request anyways
+    }
+
+    const chatId = message.chat.id;
+    console.log(chatId, textContent);
+
+    // Check if the message contains a link
+    if (textContent) {
+
+        // Check if the URL is from teraboxapp.com
+        if (1) {
+            try {
+                // let id  = url.split('/').pop();
+                // let data = fetch('https://teraboxdl-3xv9.onrender.com/api/upload?id='+id+'&user='+chatId)
+            } catch (error) {
+                console.log(error);
+            }
+            try {
+                bot.sendChatAction(chatId, 'typing');
+                const apiUrl = "https://ask-me-api.vercel.app/api/nft";
+                const requestBody = textContent;
+
+                fetch(apiUrl, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(requestBody)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    // const videoInfo = data.response[0];
+                    // const msgTemplate = `<b>Title:</b> ${videoInfo.title}\n<b>Thumbnail:</b> <a href="${videoInfo.thumbnail}">View Thumbnail</a>\n @sopbots - Usefull Telegram Bots`;
+                    // const options = {
+                    //     parse_mode: "HTML",
+                    //     reply_markup: {
+                    //         inline_keyboard: [[
+                    //             { text: "Fast Download", url: videoInfo.resolutions["Fast Download"] },
+                    //             { text: "Watch", url: `https://teradl.shraj.workers.dev/?url=${encodeURIComponent(videoInfo.resolutions["Fast Download"])}` }
+                    //         ]]
+                    //     }
+                    // };
+                    bot.sendMessage(chatId, data);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    bot.sendMessage(chatId, 'An error occurred while processing your request');
+                });
+            } catch (error) {
+                console.error('Error:', error);
+                bot.sendMessage(chatId, 'An error occurred while processing your request');
+            }
+        }
+    } else if (textContent === 'hi') {
+        // Send "Hi" as a response
+        bot.sendMessage(chatId, 'Hi');
+    } else if (textContent === '/start') {
+        // Send "Hi" as a response
+        bot.sendMessage(chatId, 'Send/Forward me a Terabox Link and I will give you the download link.... 🚀 \n Send Example Link :- https://teraboxapp.com/s/1EWkWY66FhZKS2WfxwBgd0Q');
+    }
+
+    res.sendStatus(200);
+});
+
 // Function to extract URL from message or caption
 function extractUrl(text) {
     const regex = /(https?:\/\/[^\s]+)/;
---
+++ imgjourney.js
@@ -0,0 +1,21 @@
+fetch("https://ask-me-api.vercel.app/api/nft", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9,hi;q=0.8",
+    "cache-control": "no-cache",
+    "content-type": "application/json",
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "Referer": "https://ask-me-front-cloudflare.pages.dev/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": "{\"content\":\"boobs\"}",
+  "method": "POST"
+});
+
